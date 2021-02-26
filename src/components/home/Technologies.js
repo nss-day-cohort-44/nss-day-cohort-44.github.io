1d52cddfe98efac06d2b84013a9fdc42b3d1604f
Benjamin Schweizer <bschweiz@gmail.com>
2021-02-26 10:18:53 -0600

    changing order of techs to order we learned them in according to personal memory

--- src/components/home/Technologies.js
+++ src/components/home/Technologies.js
@@ -11,22 +11,6 @@ const Technologies = () => (
             <h2 id="techTitle">Technologies</h2><br />
             <div className="technologiesContainer">
                 <div className="cardContainer">
-                    <img className="techImage" src="/techs/JS.svg" alt="JavaScript Icon"></img>
-                    <div className="technologyText">Javascript</div>
-                </div>
-                <div className="cardContainer">
-                    <img className="techImage" src="/techs/react.svg" alt="React Icon"></img>
-                    <div className="technologyText">React JS</div>
-                </div>
-                <div className="cardContainer">
-                    <img className="techImage" src="/techs/Python_logo.svg" alt="Python Icon"></img>
-                    <div className="technologyText">Python</div>
-                </div>
-                <div className="cardContainer">
-                    <img className="techImage" src="/techs/django.png" alt="Django Icon"></img>
-                    <div className="technologyText">Django</div>
-                </div>
-                <div className="cardContainer">
                     <img className="techImage" src="/techs/octocat.png" alt="GitHub Icon"></img>
                     <div className="technologyText">GitHub</div>
                 </div>
@@ -43,24 +27,40 @@ const Technologies = () => (
                     <div className="technologyText">Bootstrap</div>
                 </div>
                 <div className="cardContainer">
-                    <img className="techImage" src="https://nss-day-cohort-34.github.io/Class-Website/images/techs/npm.png" alt="NPM Icon"></img>
-                    <div className="technologyText">NPM</div>
+                    <img className="techImage" src="/techs/JS.svg" alt="JavaScript Icon"></img>
+                    <div className="technologyText">Javascript</div>
                 </div>
                 <div className="cardContainer">
-                    <img className="techImage" src="/techs/SQL.png" alt="SQL Icon"></img>
-                    <div className="technologyText">SQL</div>
+                    <img className="techImage" src="/techs/react.svg" alt="React Icon"></img>
+                    <div className="technologyText">React JS</div>
+                </div>
+                <div className="cardContainer">
+                    <img className="techImage" src="/techs/postman.png" alt="Postman Icon"></img>
+                    <div className="technologyText">Postman</div>
                 </div>
                 <div className="cardContainer">
                     <img className="techImage" src="/techs/firebaseSquare.png" alt="Firebase Icon"></img>
                     <div className="technologyText">Firebase</div>
                 </div>
                 <div className="cardContainer">
-                    <img className="techImage" src="https://nss-day-cohort-34.github.io/Class-Website/images/techs/vsc.png" alt="VSCode Icon"></img>
-                    <div className="technologyText">Visual Studio Code</div>
+                    <img className="techImage" src="https://nss-day-cohort-34.github.io/Class-Website/images/techs/npm.png" alt="NPM Icon"></img>
+                    <div className="technologyText">NPM</div>
                 </div>
                 <div className="cardContainer">
-                    <img className="techImage" src="/techs/postman.png" alt="Postman Icon"></img>
-                    <div className="technologyText">Postman</div>
+                    <img className="techImage" src="/techs/Python_logo.svg" alt="Python Icon"></img>
+                    <div className="technologyText">Python</div>
+                </div>
+                <div className="cardContainer">
+                    <img className="techImage" src="/techs/django.png" alt="Django Icon"></img>
+                    <div className="technologyText">Django</div>
+                </div>
+                <div className="cardContainer">
+                    <img className="techImage" src="/techs/SQL.png" alt="SQL Icon"></img>
+                    <div className="technologyText">SQL</div>
+                </div>
+                <div className="cardContainer">
+                    <img className="techImage" src="/techs/VScode.png" alt="VSCode Icon"></img>
+                    <div className="technologyText">Visual Studio Code</div>
                 </div>
             </div>
         </section>
